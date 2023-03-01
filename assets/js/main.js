9214595db5546290e50ec456db77b5667861dc3a
Tharun kailash K <tharunkailash2020@gmail.com>
2023-03-01 09:46:45 +0530

    #commit-1

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+/*=================== Menu show y hidden===========*/
+const navMenu=document.getElementById('nav-menu'),
+    navtoggle=document.getElementById('nav-toggle'),
+    navClose=document.getElementById('nav-close')
+
+/*  ****  menu show *** */ 
+/* validate if constant exists */
+
+
+
+/* ====Menu hidden=========*/
+/* validate if constant exists */
+
+
+
+/* *************************** Remove menu mobile ******* */
+
+
+/* **************** Accordion Skills*********************** */
+
+
+/* ======================Qualification tabs====================== */
