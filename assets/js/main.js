ee6b78b84407dafa6dda4be31d78a877d02b84dc
Tharun kailash K <tharunkailash2020@gmail.com>
2023-03-06 11:31:31 +0530

    #commit-2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,95 @@
 /*=================== Menu show y hidden===========*/
 const navMenu=document.getElementById('nav-menu'),
-    navtoggle=document.getElementById('nav-toggle'),
-    navClose=document.getElementById('nav-close')
+    navToggle=document.getElementById('nav-toggle'),
+    navClose=document.getElementById('nav-close');
 
 /*  ****  menu show *** */ 
 /* validate if constant exists */
-
-
+if(navToggle){
+    navToggle.addEventListener('click',() =>{
+        navMenu.classList.add('show-menu');
+    })
+}
 
 /* ============Menu hidden=========*/
 /* validate if constant exists */
-
-
+if(navClose){
+    navClose.addEventListener('click',() =>{
+        navMenu.classList.remove('show-menu');
+    })
+}
 
 /* ******************** Remove menu mobile ******* */
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /* **************** Accordion Skills*********************** */
+const skillsContent=document.getElementById('skills__content'),
+      skillsheader = document.querySelectorAll('.skills__header')
 
+function toggleSkills(){
+    let itemClass= this.parentNode.className
+
+    for(let i=0; i<skillsContent.length; i++){
+        skillsContent[i].className= 'skills__content skills__close'
+    } 
+    if(itemClass=== 'skills__content skills__close'){
+        this.parentNode.className= 'skills__content skills__open'
+    }
+}
+
+skillsheader.forEach((el) => {
+    el.addEventListener('click',toggleSkills)
+})
 
 /* ======================Qualification tabs====================== */
+const tabs= document.querySelectorAll('[data-target]'),
+      tabContents= document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target=document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
+
+
+
+/*========================Service Modal=========================== */
+
+
+
+
+/*==========================portfolio swiper====================== */
+
+
+
+
+/*====================testimonial======================= */
+
+
+
+/*=================Scroll Sections Active Link============== */
+
+
+
+/*=============Change Background Header================= */
+
+
+
